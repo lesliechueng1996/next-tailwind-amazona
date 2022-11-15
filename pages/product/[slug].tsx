5a642e1cb7a98ae78772390c512df96d2628ae85
LeslieCheung <lesliechueng1996@icloud.com>
2022-11-15 23:24:57 +0800

    add product detail page

---
+++ pages/product/[slug].tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
+import data from '../../utils/data';
+
+export default function ProductDetail() {
+  const router = useRouter();
+  const { slug } = router.query;
+  const product = data.products.find((item) => item.slug === slug);
+
+  if (!product) {
+    return <div>No Product</div>;
+  }
+
+  return (
+    <Layout title={product.name}>
+      <div>
+        <div className="py-3 font-bold">
+          <Link href="/">back to products</Link>
+        </div>
+        <div className="grid md:grid-cols-4 md:gap-3">
+          <div className="md:col-span-2">
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={640}
+              height={640}
+            />
+          </div>
+          <div>
+            <ul>
+              <li className="text-lg">{product.name}</li>
+              <li>Category: {product.category}</li>
+              <li>Brand: {product.brand}</li>
+              <li>
+                {product.rating} of {product.numReviews} reviews
+              </li>
+              <li>Description: {product.description}</li>
+            </ul>
+          </div>
+          <div>
+            <div className="card p-5">
+              <div className="flex flex-row justify-between items-center mb-2">
+                <span>Price</span>
+                <span>${product.price}</span>
+              </div>
+              <div className="flex flex-row justify-between items-center mb-2">
+                <span>Status</span>
+                <span>
+                  {product.countInStock > 0 ? 'In stock' : 'Unavailable'}
+                </span>
+              </div>
+              <div>
+                <button className="primary-button w-full">Add to cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
